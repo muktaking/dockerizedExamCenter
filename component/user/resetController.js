5834c50d30ee500647ebf1fd08ca2568977de8b0
Yeakub Ali Sarder <yeakubinfire@gmail.com>
2019-04-02 22:12:05 +0600

    dockerized pgd

---
+++ component/user/resetController.js
@@ -0,0 +1,71 @@
+//reset controller for sending reset email
+
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+
+//importing models
+const User = require('./userModel');
+
+
+module.exports.postReset = (req,res,next)=>{
+
+    crypto.randomBytes(32, (err, buffer)=>{
+        if(err){
+            console.log(err);
+            return req.redirect('/user/login');
+        }
+
+        const token = buffer.toString('hex');
+        User.findOne({email: req.body.email})
+        .then( user=>{
+
+            if(!user){
+                return res.redirect('/'); // DO SOME RENDER PAGE
+            }
+            user.resetToken = token;
+            user.resetTokenExpiration = Date.now() + (3*60*60*1000);
+            user.save()
+            .then(saved=>{
+                res.redirect('/');
+                // create reusable transporter object using the default SMTP transport
+                let transporter = nodemailer.createTransport({
+                    host: process.env.EMAIL_HOST,
+                    port: process.env.EMAIL_PORT,
+                    secure: process.env.IS_SECURE, // true for 465, false for other ports
+                    auth: {
+                    user: process.env.EMAIL_USER, // generated ethereal user
+                    pass: process.env.EMAIL_PASSWORD // generated ethereal password
+                    },
+                    tls: {
+                        rejectUnauthorized: false
+                    }
+                });
+
+                // setup email data with unicode symbols
+                let mailOptions = {
+                    from: `"No-reply" <${process.env.EMAIL_USER}>`, // sender address
+                    to: req.body.email, // list of receivers
+                    subject: "Reset Your Password, ", // Subject line
+                    html: `<h3>Reset Your Password</h3>
+                            <p>If you request for password reset, click this <a href="http://localhost:3000/user/reset/${token}">link</a></p>
+                    ` // html body
+                };
+                // send mail with defined transport object
+                transporter.sendMail(mailOptions,(error, info)=>{
+                    if(error){
+                        console.log(error);
+                    }
+                    console.log(info);
+                })
+            })
+            .catch(err=>{
+                console.log(err);
+            });
+        }
+        )
+        .catch(err=>{
+            console.log(err);
+        });
+
+    })
+}
---
+++ db.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const {
+    MONGO_USERNAME,
+    MONGO_PASSWORD,
+    MONGO_HOSTNAME,
+    MONGO_PORT,
+    MONGO_DB
+  } = process.env;
+
+const options = {
+useNewUrlParser: true,
+useCreateIndex: true,
+reconnectTries: Number.MAX_VALUE,
+reconnectInterval: 500, 
+connectTimeoutMS: 10000,
+};  
+
+//setting up web environment
+const PORT = process.env.PORT || 3000;
+module.exports.url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+mongoose.connect(url, options).then( function() {
+    console.log('MongoDB is connected');
+    app.listen(PORT, console.log('Connected to port: ', PORT));
+  })
+    .catch( function(err) {
+    console.log(err);
+  });
+
+const db = mongoose.connection;
+db.on('reconnectFailed', ()=> {
+    throw new Error('Connection lost with db');
+})
+
+module.exports ={
+  url
+}
