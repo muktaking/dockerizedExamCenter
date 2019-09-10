26dcf118fa9efce1c20755cec47391a5a50de86f
Md. Yeakub Sarder <yeakubinfire@gmail.com>
2019-09-10 23:29:38 +0600

    some changes

--- db.js
+++ db.js
@@ -18,7 +18,7 @@ connectTimeoutMS: 10000,
 
 //setting up web environment
 const PORT = process.env.PORT || 3000;
-const url = (APP_MONGO_USER & APP_MONGO_PASS & MONGO_HOSTNAME & MONGO_PORT & MONGO_DB) ? `mongodb://${APP_MONGO_USER}:${APP_MONGO_PASS}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin` :
+const url = (APP_MONGO_USER & APP_MONGO_PASS & MONGO_HOSTNAME & MONGO_PORT & MONGO_DB) ? `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin` :
 'mongodb://localhost:27017/myproject';
 
 console.log(url);
